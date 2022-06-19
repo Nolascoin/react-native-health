65c3e534e784832e5ff4b08d35ee131a6d1985b9
Alex Nolasco <alex.nolasco@chronwell.com>
2022-06-19 13:32:31 -0400

    debug

--- RCTAppleHealthKit/RCTAppleHealthKit.m
+++ RCTAppleHealthKit/RCTAppleHealthKit.m
@@ -772,7 +772,7 @@ RCT_EXPORT_METHOD(getClinicalRecords:(NSDictionary *)input callback:(RCTResponse
     [[NSNotificationCenter defaultCenter] postNotificationName:name
                                                     object:self
                                                   userInfo:payload];
-
+}
 
 // Will be called when this module's last listener is removed, or on dealloc.
 -(void)stopObserving {
